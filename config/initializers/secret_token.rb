fa4dc76d522a7924b51696d4c8738f75ab5fb6dc
localadmin <localadmin@db06.futureprogress.com>
2014-07-24 22:18:39 +0100

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SampleApp::Application.config.secret_key_base = 'bc79b03eef46bcc738afa2e8ec5c37845987afc34d49951183eb69f2e45c4bee791802a72d74b2d641f7f961a33c42f285fbb85d5457ec74e9bd81bde7de214a'
